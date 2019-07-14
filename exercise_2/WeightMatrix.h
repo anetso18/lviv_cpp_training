6db94dce42ea045fce667cea6ef960e08893b69a
Anna Storozhenko <anetso18@gmail.com>
2019-07-14 23:59:00 +0300

    WeightMatrix.h

---
+++ exercise_2/WeightMatrix.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+struct WeightMatrix
+
+{
+public:
+	WeightMatrix();
+	WeightMatrix(const int& vertices);
+  ~WeightMatrix();
+  std::vector<std::vector<int>> weightMatrix;
+private:
+	friend std::ostream& operator<< (std::ostream& out, const WeightMatrix& m);
+};
+ 
